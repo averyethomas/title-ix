e620df8a8e4647f85b1c5e973d6cfa9f8e8f23fb
Avery Thomas <averyethomas@gmail.com>
2018-09-09 21:23:57 -0500

    college select working, add scroll to and start us-states.json file

--- _assets/scripts.js
+++ _assets/scripts.js
@@ -1,12 +1,29 @@
 var countByState = [];
+var case_data = [];
+var resolved_cases = [];
+var active_cases = [];
 
-$.get( "http://projects.chronicle.com/titleix/api/v1/cases/?status=active", function( data ) {
+$.get( "http://projects.chronicle.com/titleix/api/v1/cases/", function( data ) {
 	
 	for(var i = 0; i < data.length; i++){
 		
+		case_data.push(data[i]);
+		
+		if(data[i].status == 'resolved') {
+			resolved_cases.push(data[i]);
+		} else if (data[i].status == 'active') {
+			active_cases.push(data[i]);
+
+		}
+		
+		if(countByState.includes(data[i].state_abbrev) == false){
+			num = 1;
+			countByState.push(data[i].state_abbrev);
+		} 
+
 /*
 		for(var i = 0; i < countByState; i++){
-			if(countByState[i].includes(data[i].state_abbrev) == false){
+			if(countByState[i].state.includes(data[i].state_abbrev) == false){
 				num = 1;
 				countByState.push({ state: data[i].state_abbrev, count: num });
 			}
@@ -20,39 +37,29 @@ $.get( "http://projects.chronicle.com/titleix/api/v1/cases/?status=active", func
 		} 
 */	
 	}
-	
 });
 
-// console.log(countByState);
+ console.log(countByState);
 
 // MAP FUNCTION
 
 var drawMap = function(){
-	var width = 960;
-	var height = 500;
 	
-	var projection = d3.geo.albersUsa()
-		.translate([width/2, height/2])
-		.scale([1000]);
-	
-	var path = d3.geo.path()
-		.projection(projection);
-		
-	var svg = d3.select("#map")
-		.append("svg")
-		.attr("width", width)
-		.attr("height", height);
 }
+
+drawMap();
 
 var selectStateData = function(state) {
 	
-	var url = 'http://projects.chronicle.com/titleix/api/v1/cases/?status=active&state_abbrev=' + state;
+	var url = 'http://projects.chronicle.com/titleix/api/v1/cases/?state_abbrev=' + state;
+	
 	
 	$.get( url, function( data ) {
+		//console.log(data);
 		
 		$('#cases-in-state').append(
 			'<h3>Active Cases In<br><span class="large">' + data[0].state_name + '</span></h3>' + 
-			'<div class="row" disabled>' +
+			'<div class="row">' +
 			'<div class="col">' +  
 			'<p><b>College Name</b></p>'+
 			'</div>' +
@@ -66,55 +73,90 @@ var selectStateData = function(state) {
 		)
 		
 		for(var i = 0; i < data.length; i++){
-		
-			$('#cases-in-state').append(
-				'<div class="row" onclick="selectCollegeData(' + data[i].college_unitid + ');">' +
-				'<div class="col">' +  
-				'<p>' + data[i].college + '</p>'+
-				'</div>' +
-				'<div class="col">' +  
-				'<p>' + data[i].sector + '</p>'+
-				'</div>' +
-				'<div class="col">' +  
-				'<p>' + data[i].opened + '</p>'+
-				'</div>' +
-				'</div>'
-			);
+			
+			if(data[i].status == 'active'){
+				$('#cases-in-state').append(
+					'<div class="row" onclick="selectCollegeData(' + data[i].college_unitid + ');">' +
+					'<div class="col">' +  
+					'<p>' + data[i].college + '</p>'+
+					'</div>' +
+					'<div class="col">' +  
+					'<p>' + data[i].sector + '</p>'+
+					'</div>' +
+					'<div class="col">' +  
+					'<p>' + data[i].opened + '</p>'+
+					'</div>' +
+					'</div>'
+				);
+			}
 		}
 	
-		// ADD SCROLL TO #cases-in-state DIV
+		$('html,body').animate({
+        	scrollTop: $("#cases-in-state").offset().top},
+        'slow');
+
 		
 	});
 }
 
-selectStateData('NC');
+selectStateData('NY');
 
 var selectCollegeData = function(id) {
 	
 	var url = 'http://projects.chronicle.com/titleix/api/v1/colleges/?college_unitid=' + id;
 	
+	var active_cases_state = [];
+	var resolved_cases_state = [];
+	
 	$.get( url, function( data ) {
 		
 		$('#cases-at-college').empty().append(
 			'<h3>Case Stats For<br><span class="large">' + data[0].college_name + '</span></h3>' +
-			'<div id="college-graph"></div>'
+			'<div id="college-graph"><div id="inner-college-graph"></div>'
 		)
-		var cases = [];
-		var resolvedCases = [];
-		var active_cases = [];
 		
+		for(var i = 0; i < active_cases.length; i++){
+			if(active_cases[i].college_unitid == id) {
+				active_cases_state.push(active_cases[i]);
+			}
+		}
+		
+		for(var i = 0; i < resolved_cases.length; i++){
+			if(resolved_cases[i].college_unitid == id) {
+				resolved_cases_state.push(resolved_cases[i]);
+			}
+		}
+		
+		var average_case_length;
+		
+		if(resolved_cases_state.length !== 0) {
+			var total_case_length = 0;
+		
+			for(var i = 0; i < resolved_cases_state.length; i++) {
+				
+				var start = Date.parse(resolved_cases_state[i].opened);
+				var end = Date.parse(resolved_cases_state[i].closed);
+				
+				var difference =  Math.round((end - start) / (1000*60*60*24));
+				total_case_length += difference;
+			}
+			
+			average_case_length = total_case_length / resolved_cases_state.length + ' Days';
+		} else {
+			average_case_length = '<i>No Resolved Case Data</i>'
+		}
+		
+/*
+		var cases = [];
+				
 		for(var i = 0; i < data[0].cases.length; i++){
 			
 			var caseUrl = 'http://projects.chronicle.com/titleix/api/v1/cases/?case_id=' + data[0].cases[i];
 						
 			$.get( caseUrl, function( data ){
+				
 				cases.push(data[0]);
-				if(data[0].status == 'resolved'){
-					resolvedCases.push(data[0]);
-				} else if (data[0].status == 'active'){
-					active_cases.push(data[0]);
-				}
-	
+				
 			});
 		}
 		
@@ -122,15 +164,117 @@ var selectCollegeData = function(id) {
 
 		// Calculate Case Length Average HERE
 		
-		
-		//drawBarGraph(resolvedCases, active_cases);
-		
+		console.log(active);
+*/			
+		drawBarGraph(resolved_cases_state.length, active_cases_state.length);
+	
 		$('#cases-at-college').append(
-			'<p><b>Total Number of Cases: </b>' + data[0].cases.length + ' <span class="divider"> </span> <b>Average Case Length: </b>' + 2 + ' Days</p>' + 
+			'<p>Total Number of Cases: ' + data[0].cases.length + ' <span class="divider"> </span>Average Case Length: ' + average_case_length + '</p>' + 
 			'<a class="btn outline" target="_blank" href="https://projects.chronicle.com/titleix/campus/' + data[0].college_name.replace("&", "-and-").replace(/\s+/g, '-').replace(/[,+()$~%.'":*?<>{}]/g,'') +'">Learn More</a>'
 		)
+		
+		$('html,body').animate({
+        	scrollTop: $("#cases-at-college").offset().top},
+        'slow');
+
 	});
 }
 
-var drawBarGraph = function(resolved, active){
+var drawBarGraph = function(value1, value2){
+	var data = [
+		{
+			"name": "Resolved Cases",
+            "value": value1,
+        },
+        {
+            "name": "Active Cases",
+            "value": value2,
+        }];
+
+    //sort bars based on value
+    data = data.sort(function (a, b) {
+        return d3.ascending(a.value, b.value);
+    })
+
+        //set up svg using margin conventions - we'll need plenty of room on the left for labels
+    var margin = {
+        top: 15,
+        right: 25,
+        bottom: 15,
+        left: 100
+    };
+
+    var width = parseInt(d3.select("#inner-college-graph").style("width")) - margin.left - margin.right,
+    	height = (parseInt(d3.select("#inner-college-graph").style("width")) * .3) - margin.top - margin.bottom;
+
+    var svg = d3.select("#inner-college-graph").append("svg")
+        //.attr("width", width + margin.left + margin.right)
+        //.attr("height", height + margin.top + margin.bottom)
+        .attr("preserveAspectRatio", "xMidYMid meet")
+        .attr("viewBox", "0 0 750 225")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([0, d3.max(data, function (d) {
+            return d.value;
+        })]);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([height, 0], .1)
+        .domain(data.map(function (d) {
+            return d.name;
+        }));
+
+    //make y axis to show bar names
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickSize(0)
+        .orient("left");
+
+    var gy = svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+
+    var bars = svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("g")
+
+    //append rects
+    bars.append("rect")
+        .attr("class", "bar")
+        .attr("y", function (d) {
+            return y(d.name);
+        })
+        .attr("height", y.rangeBand())
+        .attr("x", 0)
+        .attr("width", 0)
+        .transition()
+        .duration(1500)
+        .attr("width", function (d) {
+            return x(d.value);
+        });
+
+    //add a value label to the right of each bar
+    bars.append("text")
+        .attr("class", function (d) { if(d.value == 0){ return "label zero"; } else { return "label"; } })
+        //y position of the label is halfway down the bar
+        .attr("y", function (d) {
+            return y(d.name) + y.rangeBand() / 2 + 15;
+        })
+        //x position is 3 pixels to the right of the bar
+        .attr("x", function (d) {
+	        if(d.value == 0){ return x(d.value) + 35; }
+	        else { return x(d.value) - 15; }
+            
+        })
+        .text(function (d) {
+            return d.value;
+        });
+        
+	function resize() { }
+	
+	d3.select(window).on('resize', resize);
 }
